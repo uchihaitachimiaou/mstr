9704f9aee1026b05963017e1fccc0288941cbaee
ligno <lignographic@gmail.com>
2021-12-04 22:49:41 +0100

    adding files for githubpages

---
+++ src/component/main.component.jsx
@@ -0,0 +1,12 @@
+import {React} from 'react';
+
+const Main = function(){
+  return(
+    <div>
+      <h2>main</h2>
+      <p>main's a children component</p>
+    </div>
+  )
+};
+
+export default Main;
